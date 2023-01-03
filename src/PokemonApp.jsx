aebc89449a6a6e073f1bc25f2e0fa92be2fb4ad4
lumusitech <lumusitech@gmail.com>
2023-01-02 23:01:55 -0300

    [ADD] pokemons redux ready

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getPokemons } from './store/slices/pokemon/thunks'
+
+export const PokemonApp = () => {
+
+  const dispatch = useDispatch()
+  const { isLoading, page, pokemons } = useSelector(state => state.pokemons)
+
+  useEffect(() => {
+    dispatch(getPokemons())
+  }, [])
+
+
+  return (
+    <>
+      <h1>PokemonApp</h1>
+      <hr />
+      <span>Loading: {isLoading ? 'true' : 'false'}</span>
+      <ul>
+        {
+          pokemons.map((pokemon) => (
+            <li key={pokemon.name}>
+              {pokemon.name}
+            </li>
+          ))
+        }
+      </ul>
+
+      <button
+        onClick={() => dispatch(getPokemons(page))}
+        disabled={isLoading}
+      >
+        Next
+      </button>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import './index.css'
+import { PokemonApp } from './PokemonApp'
 
 import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      {/* <App /> */}
+      <PokemonApp />
     </React.StrictMode>
   </Provider>
   ,
